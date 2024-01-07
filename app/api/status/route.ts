1a40f62ee6c9a1ca3616e4d05688ef1a9efd0c7d
fakes1lent <77695756+fakes1lent@users.noreply.github.com>
2024-01-08 03:40:16 +0500

    chore: add api and helpers

---
+++ app/api/status/route.ts
@@ -0,0 +1,10 @@
+import { SuccessResponse } from "@/helpers/api/response";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest, response: NextResponse) {
+    return Response.json(
+        SuccessResponse({
+            status: "alive",
+        }),
+    );
+}
