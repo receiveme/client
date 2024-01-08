42376c6fd9f0d841e6d9ec75846092348ae189bc
nickmura <nickmuradev@gmail.com>
2024-01-08 01:05:40 -0700

    fixed navbar, changed layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { Archivo } from "next/font/google";
 import Providers from "../providers";
 
 import "./globals.css";
+import Navbar from "@/components/navbar";
+import Toast from "@/components/toast";
 
 const archivo = Archivo({ subsets: ["latin"] });
 
@@ -49,7 +51,15 @@ export default function RootLayout({
             </Head>
 
             <body className={archivo.className}>
-                <Providers>{children}</Providers>
+                <main className="min-h-screen flex justify-center">
+                    <div className="py-4 px-4 lg:px-0 lg:max-w-[85%] w-full">
+                        <Providers>
+                            <Navbar />
+                            {children}
+                        </Providers>
+                        {/* <Toast show /> */}
+                    </div>
+                </main>
             </body>
         </html>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Navbar from "@/components/navbar";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
     return (
         <main>
-            
+            <Navbar></Navbar>
             <Link href="/login" className="text-green-500 underline block">
                 /login
             </Link>
