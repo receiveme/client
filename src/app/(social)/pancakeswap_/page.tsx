a50abc78d17278ae43f61c1bc86a0ac9a67f7fe8
fakes1lent <77695756+fakes1lent@users.noreply.github.com>
2024-03-05 21:10:38 +0000

    update

--- src/app/(social)/pancakeswap_/page.tsx
+++ src/app/(social)/pancakeswap_/page.tsx
@@ -2,9 +2,10 @@
 import "../../globals.css";
 import toast from "react-hot-toast";
 
-import CAKEABI from './cakeabi.json'
+import CAKEABI from "./cakeabi.json";
 
-import { Contract, ethers } from 'ethers'
+import { Contract, ethers } from "ethers";
+import { IconChevronRight } from "@tabler/icons-react";
 
 export default function PancakeSwap() {
     function copy(text: string) {
@@ -16,29 +17,19 @@ export default function PancakeSwap() {
             toast.error("Address cannot be copied");
         }
     }
-    async function checkCAKEBNB(address:string) {
+    async function checkCAKEBNB(address: string) {
+        let provider = new ethers.JsonRpcProvider(
+            "https://binance.nodereal.io",
+        );
+        let contract = new Contract(
+            "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82",
+            CAKEABI,
+            provider,
+        );
+        const balance = await contract.balanceOf(address);
+        console.log(balance);
+    }
 
-            let provider = new ethers.JsonRpcProvider('https://binance.nodereal.io');
-            // let signer = provider.getSigner();
-            let contract = new Contract('0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82', CAKEABI, provider)
-            const balance = await contract.balanceOf(address)
-            console.log(balance)
-
-    } 
-
-
-    
-    async function checkCAKEopBNB(address:string) {
-        try {
-            let provider = new ethers.JsonRpcProvider('https://opbnb-mainnet.nodereal.io/v1/64a9df0874fb4a93b9d0a3849de012d3');
-            let signer = provider.getSigner();
-            let contract = new Contract('0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82', CAKEABI, provider)
-            const balance = await contract.balanceOf(address)
-            console.log(balance)
-        } catch (e) {
-            console.log(e)
-        }
-    } 
     return (
         <div
             className="
@@ -58,23 +49,30 @@ export default function PancakeSwap() {
                     ></div>
                     <div className="relative z-10 h-full flex flex-col justify-end p-4">
                         <h1 className="font-inter font-semibold text-2xl">
-                            <a href="https://pancakeswap.finance/" target="_blank">
+                            <a
+                                href="https://pancakeswap.finance/"
+                                target="_blank"
+                            >
                                 PancakeSwap
                             </a>
                         </h1>
                         <p className="text-gray-300">
-                            PancakeSwap, a DeFi platform on Binance Smart Chain, is a decentralized exchange (DEX) enabling users to swap cryptocurrencies, provide liquidity, and earn CAKE tokens. Dive into decentralized finance with PancakeSwap's seamless, rewarding experience!
+                            PancakeSwap, a DeFi platform on Binance Smart Chain,
+                            is a decentralized exchange (DEX) enabling users to
+                            swap cryptocurrencies, provide liquidity, and earn
+                            CAKE tokens. Dive into decentralized finance with
+                            PancakeSwap's seamless, rewarding experience!
                         </p>
                     </div>
                 </div>
                 <div className="flex flex-col">
                     <a
-                        href="https://awpswap.io"
+                        href="https://pancakeswap.finance/"
                         target="_blank"
                         className="p-4 transition flex items-center hover:bg-[#ffffff2b]"
                     >
                         <img className="w-6" src="/icons/home.svg" />
-                        <span className="ml-3">awpswap.io</span>
+                        <span className="ml-3">pancakeswap.finance</span>
                     </a>
                     <a
                         href="https://twitter.com"
@@ -99,41 +97,23 @@ export default function PancakeSwap() {
                 <div className="flex flex-col">
                     <div className="p-4 transition flex justify-between items-center hover:bg-[#ffffff2b] rounded-tl-xl rounded-tr-xl">
                         <div className="flex items-center">
-                            <img className="h-6" src="/img/3p/eth.png" />
+                            <img className="h-6" src="/img/3p/cake.png" />
                             <span className="break-all ml-3 mr-4">
-                                0x60A3166452fF240F5F521C0EFD3259Af56179e63
+                                Check if you have interacted with $CAKE
                             </span>
                         </div>
                         <div>
                             <button
                                 onClick={() =>
-                                    checkCAKEBNB('0xE70b8dc28E795738A772379E9D456E7d74f50aB5')
+                                    checkCAKEBNB(
+                                        "0xE70b8dc28E795738A772379E9D456E7d74f50aB5",
+                                    )
                                 }
                                 className="
                                     flex items-center justify-center h-7 w-7
                                     bg-white rounded-md transition hover:bg-gray-200 hover:scale-110"
                             >
-                                <img src="/icons/copy.svg" className="h-4" />
-                            </button>
-                        </div>
-                    </div>
-                    <div className="p-4 transition flex justify-between items-center hover:bg-[#ffffff2b] rounded-bl-xl rounded-br-xl">
-                        <div className="flex items-center">
-                            <img className="h-6" src="/img/3p/tron.png" />
-                            <span className="break-all ml-3 mr-4">
-                                TSb4jDB4wY1VExgtZydJF6TKFuwJKdDrK8
-                            </span>
-                        </div>
-                        <div>
-                            <button
-                                onClick={() =>
-                                    copy("TSb4jDB4wY1VExgtZydJF6TKFuwJKdDrK8")
-                                }
-                                className="
-                                    flex items-center justify-center h-7 w-7
-                                    bg-white rounded-md transition hover:bg-gray-200 hover:scale-110"
-                            >
-                                <img src="/icons/copy.svg" className="h-4" />
+                                <IconChevronRight fill="black" />
                             </button>
                         </div>
                     </div>
