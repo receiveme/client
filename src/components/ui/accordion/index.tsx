dffbc799364eab71a8b34b27995c35d3e560dd1d
sabinbaniya <dummymail@tutanota.de>
2024-06-09 16:32:12 +0545

    feat: create `Accordion` component

---
+++ src/components/ui/accordion/index.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import * as React from "react";
+import * as AccordionPrimitive from "@radix-ui/react-accordion";
+
+import { cn } from "@/src/lib/utils/cn";
+import { IconChevronDown } from "@tabler/icons-react";
+
+const Accordion = AccordionPrimitive.Root;
+
+const AccordionItem = React.forwardRef<
+    React.ElementRef<typeof AccordionPrimitive.Item>,
+    React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
+>(({ className, ...props }, ref) => (
+    <AccordionPrimitive.Item
+        ref={ref}
+        className={cn("mb-4 ", className)}
+        {...props}
+    />
+));
+AccordionItem.displayName = "AccordionItem";
+
+const AccordionTrigger = React.forwardRef<
+    React.ElementRef<typeof AccordionPrimitive.Trigger>,
+    React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
+>(({ className, children, ...props }, ref) => (
+    <AccordionPrimitive.Header className="flex">
+        <AccordionPrimitive.Trigger
+            ref={ref}
+            className={cn(
+                "flex flex-1 bg-white border border-gray-400 rounded-xl p-4 items-center justify-between text-lg font-medium transition-all [&[data-state=open]>svg]:rotate-180",
+                className,
+            )}
+            {...props}
+        >
+            {children}
+            <span className="bg-gray-800 rounded-full p-1">
+                <IconChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200 text-white" />
+            </span>
+        </AccordionPrimitive.Trigger>
+    </AccordionPrimitive.Header>
+));
+AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName;
+
+const AccordionContent = React.forwardRef<
+    React.ElementRef<typeof AccordionPrimitive.Content>,
+    React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+    <AccordionPrimitive.Content
+        ref={ref}
+        className="overflow-hidden text-base transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
+        {...props}
+    >
+        <div className={cn("p-4", className)}>{children}</div>
+    </AccordionPrimitive.Content>
+));
+
+AccordionContent.displayName = AccordionPrimitive.Content.displayName;
+
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,21 @@ const config: Config = {
             },
             colors: {
                 primary: "#9D44B5",
+                "blur-circle": "rgba(247, 174, 248, 0.8)",
+            },
+            keyframes: {
+                "accordion-down": {
+                    from: { height: "0" },
+                    to: { height: "var(--radix-accordion-content-height)" },
+                },
+                "accordion-up": {
+                    from: { height: "var(--radix-accordion-content-height)" },
+                    to: { height: "0" },
+                },
+            },
+            animation: {
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
             },
         },
         screens: {
