e908ed09e24b26b52ad1cf07a3cc18c2d87d6975
sabinbaniya <dummymail@tutanota.de>
2024-06-10 08:08:58 +0545

    feat: add accordion chevron animation

--- src/components/ui/accordion/index.tsx
+++ src/components/ui/accordion/index.tsx
@@ -28,14 +28,14 @@ const AccordionTrigger = React.forwardRef<
         <AccordionPrimitive.Trigger
             ref={ref}
             className={cn(
-                "flex flex-1 bg-white border border-gray-400 rounded-xl p-4 items-center justify-between text-lg font-medium transition-all [&[data-state=open]>svg]:rotate-180",
+                "AccordionTrigger flex flex-1 bg-white border border-gray-400 rounded-xl p-4 items-center justify-between text-lg font-medium transition-all [&[data-state=open]>svg]:rotate-180",
                 className,
             )}
             {...props}
         >
             {children}
-            <span className="bg-gray-800 rounded-full p-1">
-                <IconChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200 text-white" />
+            <span className="bg-gray-800 rounded-full p-1 transition-transform duration-200">
+                <IconChevronDown className="h-4 w-4 shrink-0 text-white" />
             </span>
         </AccordionPrimitive.Trigger>
     </AccordionPrimitive.Header>
