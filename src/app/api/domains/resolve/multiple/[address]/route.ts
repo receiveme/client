3c605a7505b06274f946258cb0f3176fc672f715
sabinbaniya <dummymail@tutanota.de>
2024-06-14 12:07:08 +0545

    feat: only return domains ending with `.polygon` for matic chain

--- src/app/api/domains/resolve/multiple/[address]/route.ts
+++ src/app/api/domains/resolve/multiple/[address]/route.ts
@@ -25,9 +25,17 @@ export const GET = async (
         if (json?.data?.length > 0) {
             const domains = json?.data;
 
-            const domainOnRequestedChain = domains.find(
+            let domainOnRequestedChain = domains.find(
                 (d: any) => d.meta.blockchain.toLowerCase() === requestedChain,
             );
+
+            if (requestedChain === "matic") {
+                domainOnRequestedChain = domains.find(
+                    (d: any) =>
+                        d.meta.blockchain.toLowerCase() === requestedChain &&
+                        d.meta.domain.endsWith(".polygon"),
+                );
+            }
 
             return NextResponse.json({
                 success: true,
