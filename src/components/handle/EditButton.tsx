05d987831a13cc74b6f54ce73dfe9aa9f1d68e86
sabinbaniya <dummymail@tutanota.de>
2024-06-15 09:17:26 +0545

    fix: edit button showing on all `/[handle]`

--- src/components/handle/EditButton.tsx
+++ src/components/handle/EditButton.tsx
@@ -12,7 +12,7 @@ export default function EditHandleButton({ handle }: any) {
             <Link
                 href="/dashboard"
                 className={`mb-6 flex w-full items-center justify-center gap-1 rounded-lg bg-indigo-600 py-2 text-sm font-bold text-white transition hover:bg-indigo-700 ${
-                    handle !== appState.userData?.handle ? "hidden" : ""
+                    handle !== appState.userData?.handle ? "!hidden" : ""
                 }`}
             >
                 <IconEdit size="16" />
