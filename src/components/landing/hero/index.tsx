e4b22122ea3958eba77c4662aa66b7d33b5dbaad
Destiny Aigbe <aigbedestinyic@gmail.com>
2024-06-19 10:33:03 +0100

    feat: add user interactions

--- src/components/landing/hero/index.tsx
+++ src/components/landing/hero/index.tsx
@@ -1,5 +1,6 @@
 import { Button } from "../../ui/button";
 import { Chip } from "../../ui/chip";
+import HeroAnimation from "./HeroAnimation";
 import { NameSearch } from "./name-search";
 
 export const HeroSection = () => {
@@ -32,16 +33,7 @@ export const HeroSection = () => {
                     </div>
                 </div>
                 <div className="basis-[45%] relative w-[70%] ml-full lg:ml-0 lg:w-auto">
-                    <img
-                        src="/img/home/svg/grid.svg"
-                        alt=""
-                        className="-z-10 absolute top-0 left-0 w-[50%] lg:w-auto"
-                    />
-                    <div className="z-10 absolute -top-4 lg:top-0 right-8 lg:h-44 lg:w-44 w-36 h-36 bg-white/40 backdrop-blur-sm rounded-full shadow-black/10 shadow-inner drop-shadow-xl" />
-                    <div className="z-10 absolute bottom-0 right-20 lg:h-32 lg:w-32 h-28 w-28 bg-white/40 backdrop-blur-sm rounded-full shadow-black/10 shadow-inner drop-shadow-xl" />
-                    <div className="hero-svg-mask">
-                        <img src="/img/home/nick2.png" alt="" />
-                    </div>
+                    <HeroAnimation />
                 </div>
             </div>
         </>
