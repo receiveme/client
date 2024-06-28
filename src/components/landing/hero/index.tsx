8746ae21395e393cabd9846db63e082c00522ba0
sabinbaniya <dummymail@tutanota.de>
2024-06-28 09:05:51 +0545

    feat: update hero text, fix layout shift while heroanimation loading

--- src/components/landing/hero/index.tsx
+++ src/components/landing/hero/index.tsx
@@ -8,12 +8,12 @@ export const HeroSection = () => {
         <>
             <div className="flex flex-col lg:flex-row items-end lg:items-center justify-between gap-0 lg:gap-16">
                 <div className="basis-[55%]">
-                    <Chip className="mb-2">Receive Me</Chip>
+                    <Chip className="mb-2 lg:mb-[-80px]">Receive Me</Chip>
                     <p
-                        className="text-4xl sm:text-5xl font-medium tracking-tight"
+                        className="text-4xl sm:text-5xl lg:text-6xl font-medium tracking-tight"
                         style={{ lineHeight: 1.345 }}
                     >
-                        We Provide Easy Solution To
+                        {/* We Provide Easy Solution To */}
                         <br className="hidden lg:block" /> Link All Of Your{" "}
                         <span className="relative whitespace-nowrap bg-primary/40 border-2 border-dashed px-3 border-primary">
                             <span className="dot1 bg-primary rounded-full h-2 w-2 absolute -top-1 -left-1"></span>
@@ -32,7 +32,7 @@ export const HeroSection = () => {
                         & help you manage your payments in one place.
                     </div>
                 </div>
-                <div className="basis-[55%] relative w-full lg:w-auto">
+                <div className="basis-[55%] relative w-full lg:w-auto lg:min-h-[555px]">
                     <HeroAnimation />
                 </div>
             </div>
