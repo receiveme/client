d53a7755702e1ab3f8469db24406a162c04395f3
sabinbaniya <dummymail@tutanota.de>
2024-07-03 17:32:52 +0545

    feat: update schema, add nonce property to user

---
+++ prisma/migrations/20240703070706_add_nonce_to_user/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - The required column `nonce` was added to the `users` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
+
+*/
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "nonce" TEXT NOT NULL;
