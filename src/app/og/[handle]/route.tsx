9f562ac4b83200a374af5dcafabfa599e1cbf9c7
sabinbaniya <dummymail@tutanota.de>
2024-08-13 21:08:56 +0545

    feat: remove default image for no handles in og

--- src/app/og/[handle]/route.tsx
+++ src/app/og/[handle]/route.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { promises } from "fs";
 import { ImageResponse } from "next/og";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -168,28 +168,29 @@ export async function GET(
         );
     } catch (e) {
         console.log(e, "e");
-        return new ImageResponse(
-            (
-                <div
-                    style={{
-                        fontSize: 40,
-                        color: "black",
-                        background: "white",
-                        width: "100%",
-                        height: "100%",
-                        padding: "50px 200px",
-                        textAlign: "center",
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}
-                >
-                    👋 receive.me
-                </div>
-            ),
-            {
-                width: 1200,
-                height: 630,
-            },
-        );
+        return NextResponse.json(null, { status: 400 });
+        // return new ImageResponse(
+        //     (
+        //         <div
+        //             style={{
+        //                 fontSize: 40,
+        //                 color: "black",
+        //                 background: "white",
+        //                 width: "100%",
+        //                 height: "100%",
+        //                 padding: "50px 200px",
+        //                 textAlign: "center",
+        //                 justifyContent: "center",
+        //                 alignItems: "center",
+        //             }}
+        //         >
+        //             👋 receive.me
+        //         </div>
+        //     ),
+        //     {
+        //         width: 1200,
+        //         height: 630,
+        //     },
+        // );
     }
 }
